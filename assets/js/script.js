78150db9c10fd935ad1dc6124a87cca9c243b757
Gökhan Kaya <0x90kh4n@gmail.com>
2023-01-17 21:57:49 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 /* gDesk - Helpdesk Ticketing Software */
+
+$(function() {
+
+    $.ajaxSetup({ cache: false });
+
+    $('form').submit(function() {
+        $(this).find('button[type="submit"]')
+            .html('<span class="spinner-border spinner-border-sm" role="status"></span>')
+            .prop('disabled', true);
+    });
+
+});
