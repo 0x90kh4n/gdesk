e32ab9c76e53f96f7ada150f16ccffd8e37d5926
Gökhan Kaya <0x90kh4n@gmail.com>
2023-01-17 22:03:27 +0300

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * gDesk - Helpdesk Ticketing Software
+ * @author Gökhan Kaya <0x90kh4n@gmail.com>
+ */
+
+define('BASE_URL', 'http://localhost/gdesk/');
+define('TIMEZONE', 'Europe/Istanbul');
+
+// Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'gdesk');
+
+// SMTP
+define('SMTP_HOST', 'mail.yourdomain.com');
+define('SMTP_USER', 'noreply@yourdomain.com');
+define('SMTP_PASS', 'password');
+define('SMTP_PORT', '25');
+
+// Google reCaptcha
+define('RECAPTCHA_SITE_KEY', '');
+define('RECAPTCHA_SECRET_KEY', '');
